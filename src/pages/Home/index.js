8eaf42b227c08a622018009c1d0a7730510b400c
André Tronca <andretronca@gmail.com>
2019-05-28 21:35:12 -0300

    refresh list and list styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,26 +9,52 @@ class Home extends Component{
     }
 
     componentDidMount(){
-        this.getBooks()
+        // sends get request to obtain a book list
+        this.refreshBooks();
     }
 
-    async getBooks (){
+    inputChanged = (target) => {
+        console.log(localStorage);
+        console.log(JSON.parse(localStorage.books));
+    }
+
+    refreshBooks = async () => {
         this.setState({isLoading: true});
 
         const { data } = await api.get('people/george08/lists.json');
 
-        this.setState({ books: data.entries, isLoading: false });
+        await data.entries.map(book => {
+            return book.read = false
+        });
+
+        await this.setState({ books: data.entries, isLoading: false });
+
+        localStorage.setItem('books', JSON.stringify(this.state.books))
     }
 
     render(){
         const { isLoading, books } = this.state
         return (
             <div className="container">
-                <h1>My Books</h1>
-                <div className={`list ${isLoading ? 'loading' : null}`}>
-                    {!isLoading && books.map(book => (
-                        <div>{book.name}</div>
-                    ))}
+                <div className="box">
+                    <h1>My Books</h1>
+                    {isLoading &&
+                        <div className="loading">Loading Books...</div>
+                    }
+                    <div className="list">
+                        {!isLoading
+                            && books.map((book, index) => (
+                                <label key={index} htmlFor={`book-${index}`} className="book">
+                                    <input type="checkbox" name="read" id={`book-${index}`} onChange={(ev) => this.inputChanged(ev.target)}/>
+                                    <span className="checkmark"></span>
+                                    <span className="text">{book.name}</span>
+                                </label>
+                            ))
+                        }
+                    </div>
+                    {!isLoading &&
+                        <button className="common-button" type="submit" onClick={this.refreshBooks}>Refresh Books</button>
+                    }
                 </div>
             </div>
         )
