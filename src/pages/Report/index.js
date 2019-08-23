ed2093fcc8bcccfb07384edfa7075c61bd394261
André Tronca <andretronca@gmail.com>
2019-08-22 22:13:47 -0300

    create report page

---
+++ src/pages/Report/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.sass";
+
+class Report extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="box">
+          <h1>Annual Report</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Report;
