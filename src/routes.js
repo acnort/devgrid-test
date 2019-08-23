86b4d480c44876dec6f142ffb683844d5de8c9ab
André Tronca <andretronca@gmail.com>
2019-08-22 22:14:32 -0300

    set report route

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Home } from './pages';
-import { Template } from './components';
+import { Home, Report } from "./pages";
+import { Template } from "./components";
 
 const Routes = () => (
-    <Router>
-        <Template>
-            <Switch>
-                <Route path="/" exact={true} component={Home}/>
-            </Switch>
-        </Template>
-    </Router>
-)
+  <Router>
+    <Template>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/report" exact={true} component={Report} />
+      </Switch>
+    </Template>
+  </Router>
+);
 
 export default Routes;
